da666a5fa6b731889d0aa538546a4ccd71c93725
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-12-11 10:41:46 +0530

    Create ambot.py

---
+++ ambot.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && rm -rf SiestaRobot && git clone https://github.com/Avonplays/SiestaRobot && cd SiestaRobot && python3 -m SiestaRobot")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
